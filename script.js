77cbe582e287342353a0c1654781525016d5d5cb
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-04-21 23:49:32 -0600

    Displays date & current weather info

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ var lon;
 var APIKey = "41adf6f71fd0597dbaad07a430a610c9";
 var cityName;
 var optionsNamesArr = [];
+var mainW = [];
+var currentDay = dayjs().format('dddd, MMMM DD, YYYY.');
+console.log("currentDay: "+ currentDay);
+document.querySelector("#today").textContent = currentDay;
 
 //  SUBMIT BUTTON 
 document.querySelector("#submit").addEventListener("click", function(event){
@@ -38,7 +42,6 @@ function getCoordApi() {
         var optionsNames = data[i].country+" - "+data[i].name+" ("+data[i].state+")";
         document.getElementById(i).textContent= optionsNames;
         optionsNamesArr[i]=optionsNames;
-        console.log("optionsNamesArr: "+optionsNamesArr);
       }
 
       clickOptions(data,optionsNamesArr)
@@ -55,7 +58,7 @@ function clickOptions(data,optionsNamesArr){
   var saveButton = $('.options');
   saveButton.on('click', function (event) {
     var theId = this.id;
-    console.log("the id!:"+theId);
+    // console.log("the id!:"+theId);
     lat=data[theId].lat;
     lon=data[theId].lon;
     console.log("lat:"+lat);
@@ -80,7 +83,7 @@ function erraseOptions(){
   document.querySelector(".optionsBox").style.display = "none";
   for (n=0;n<optionsNamesArr.length;n++){
     optionsNamesArr.shift();
-    console.log("optionsNamesArr: "+optionsNamesArr);
+    console.log(" borrar optionsNamesArr: "+optionsNamesArr);
   }
 
 }
@@ -89,7 +92,7 @@ function erraseOptions(){
 function getWeatherApi(lat,lon,chosen) {
   console.log("   getweatherApi()");
   // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=41adf6f71fd0597dbaad07a430a610c9" ;
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=41adf6f71fd0597dbaad07a430a610c9&units=metric" ;
 
   fetch(queryURL)
     .then(function (response) {
@@ -99,17 +102,31 @@ function getWeatherApi(lat,lon,chosen) {
       console.log("Data from api 2");
       console.log(data);
 
+      // mainW[0] = data.list[0].weather[0].description;
+      // console.log(mainw);
+      // var wet = data.list[0].weather[0].description;
+      // document.querySelector("#stats").textContent=wet;
+
+      //uncommented, this works!
+      // document.querySelector("#mainW").textContent=data.list[0].weather[0].description;
+      // document.querySelector("#temp").textContent="Temperature: "+data.list[0].main.temp+" °C";
+      // document.querySelector("#wind").textContent="Wind: "+data.list[0].wind.speed+" m/sec";
+      // document.querySelector("#hum").textContent="Humidity: "+data.list[0].main.humidity+" %";
+      displayInfo(chosen,data)
+
     });
+
   
   displayInfo(chosen)
 }
 
-function displayInfo(chosen){
+function displayInfo(chosen,data){
   console.log("   displayInfo()");
-  document.querySelector("#forecast").textContent=chosen;
-  var currentDay = dayjs().format('dddd, MMMM DD, YYYY.');
-  console.log("currentDay: "+currentDay);
-  // document.querySelector("#currentDay").textContent="cpmyent";
-  // $('#currentDay').text(currentDay);
+  document.querySelector("#cityName").textContent=chosen;
+  document.querySelector("#mainW").textContent=data.list[0].weather[0].description;
+  document.querySelector("#temp").textContent="Temperature: "+data.list[0].main.temp+" °C";
+  document.querySelector("#wind").textContent="Wind: "+data.list[0].wind.speed+" m/sec";
+  document.querySelector("#hum").textContent="Humidity: "+data.list[0].main.humidity+" %";
+  
 
 }
