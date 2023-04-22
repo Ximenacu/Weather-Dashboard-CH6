b14e66eeab31fbf3046197ffeff3f57e4734e3b0
Ximena Fernández del Castillo Cu <you@example.comximenacuu@gmail.com>
2023-04-22 15:49:56 -0600

    Creates cards with 5 future days weather info

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var APIKey = "41adf6f71fd0597dbaad07a430a610c9";
 var cityName;
 var optionsNamesArr = [];
 var mainW = [];
+var date5=[];
+var temp=[];
+var wind=[];
+var hum=[] ;
 var currentDay = dayjs().format('dddd, MMMM DD, YYYY.');
 console.log("currentDay: "+ currentDay);
 document.querySelector("#today").textContent = currentDay;
@@ -127,6 +131,38 @@ function displayInfo(chosen,data){
   document.querySelector("#temp").textContent="Temperature: "+data.list[0].main.temp+" °C";
   document.querySelector("#wind").textContent="Wind: "+data.list[0].wind.speed+" m/sec";
   document.querySelector("#hum").textContent="Humidity: "+data.list[0].main.humidity+" %";
-  
 
+  for (i=0;i<5;i++){
+    var indexs =[0,10,20,30,39];
+    date5[i] = data.list[indexs[i]].dt_txt;
+    mainW[i] =data.list[indexs[i]].weather[0].description;
+    temp[i] ="Temperature: "+data.list[indexs[i]].main.temp+" °C";
+    wind[i] ="Wind: "+data.list[indexs[i]].wind.speed+" m/sec";
+    hum[i] = "Humidity: "+data.list[indexs[i]].main.humidity+" %";
+
+    document.querySelector("#future"+i).textContent=date5[i]
+    appends(i,"p","mainWeatherF",mainW,("#future"+i));
+    appends(i,"p","temperatureF",temp,("#future"+i));
+    appends(i,"p","windF",wind,("#future"+i));
+    appends(i,"p","humF",hum,("#future"+i));
+    // document.querySelector("#future"+i).appendChild()mainW[i];
+    // document.querySelector("#future"+i).appendChild()temp[i] ;
+    // document.querySelector("#future"+i).appendChild()wind[i];
+    // document.querySelector("#future"+i).textContent=hum[i];
+
+  }
+  
+  console.log("date5: "+date5);
+
+}
+
+function appends(i,Elname,className,content,selecName){
+  // Elname -  element type (string) eg: "div"
+  // classname - new element class (string) eg: "temp"
+  // content -  content to be appended
+  // selecName - name of parent element (strin) eg: "#history"
+  var varname = document.createElement(Elname);
+  varname.setAttribute("class", className);
+  varname.textContent= content[i];
+  document.querySelector(selecName).appendChild(varname);
 }
